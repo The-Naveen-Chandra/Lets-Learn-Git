c8d637a4e4a9dcb60eb4c25507df51682e789bfd
Naveen Chandra <the.naveenchandra@gmail.com>
2022-11-16 20:54:16 +0530

    Implementation of difference

--- Diff.java
+++ Diff.java
@@ -1,3 +1,9 @@
 public class Diff {
-    
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = a-b;
+
+        System.out.println("Diff :"+ c);
+    }
 }
